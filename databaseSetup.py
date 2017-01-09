dc9bb98004586eed0c061b60fbda0208fe7a57fa
Luo Rock <luorock@dhcp-128-189-215-44.ubcsecure.wireless.ubc.ca>
2017-01-08 16:47:05 -0800

    first commit, added databse setup query

---
+++ databaseSetup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import sqlite3
+
+conn = sqlite3.connect('fitnessTracker.db')
+
+print("Opened database successfully");
+
+con.execute('''DROP TABLE IF EXISTS WORKOUTRECORDS)
+#A record is this format: (id, workoutdate, workoutLength, workoutType, weight)
+
+conn.execute('''CREATE TABLE WORKOUTRECORDS
+    (ID INT PRIMARY KEY     NOT NULL,
+    WORKOUTDATE            TEXT    NOT NULL,
+    WORKOUTLENGTH   INT     NOT NULL,
+    WORKOUTTYPE     TEXT    NOT NULL,
+    WEIGHT          INT);''')
+
+print("Table created successfully");
+
+conn.close()
